6e2e3a61163437986da14d40bf4e3a72605e44e7
Sylvain Cartier <asylvain10@cegetel.net>
2017-03-29 09:10:09 +0200

    modif css button menu

--- src/modules/Menu/ButtonMenu.js
+++ src/modules/Menu/ButtonMenu.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './ButtonMenu.css';
+import { Icon } from 'react-onsenui'
 
 class ButtonMenu extends React.Component {
     render() {
         return(
          <div className='ButtonMenu'>
-            {this.props.texte}
+            <div className='divText'>{this.props.texte}</div>
+            <div className='divIcon'><Icon icon={this.props.icons}></Icon></div>
         </div>
         )
     }
